2a0ef2f1074521c8c0b61d0bff0f8f8910c2643d
enspiregaurav <enspire.gaurav@gmail.com>
2021-05-13 22:17:40 +0545

    spell corrected

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚀</text></svg>"
         />
 
-        <title>React Components List</title>
+        <title>React Component List</title>
       </Head>
       <Component {...pageProps} />
     </>
