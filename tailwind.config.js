96a5eb61c0a0d3e83ad5c5f8920423b6cb85e728
enspiregaurav <enspire.gaurav@gmail.com>
2021-05-13 22:08:37 +0545

    tailwind config changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
-console.log(
-  "----------",
-  process.env.node_env === "production"
-    ? ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"]
-    : []
-);
+console.log("----------", JSON.stringify(process.env, null, 2));
 module.exports = {
   purge: {
-    enabled: process.env.node_env === "production",
+    // enabled: process.env.node_env === "production",
+    enabled: true,
     content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
   },
   darkMode: false, // or 'media' or 'class'
