4dfcae4c05781faed331625c80e579001b0a77fc
enspiregaurav <enspire.gaurav@gmail.com>
2021-05-13 21:58:33 +0545

    purge unneeded css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
-console.log("----------", process.env.node_env);
+console.log(
+  "----------",
+  process.env.node_env === "production"
+    ? ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"]
+    : []
+);
 module.exports = {
-  purge:
-    process.env.node_env === "production"
-      ? ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"]
-      : [],
+  purge: {
+    enabled: process.env.node_env === "production",
+    content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
